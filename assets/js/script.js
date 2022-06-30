a1124b78078862a5e9005e0058e76644c1f8da08
cultstatue <saltedchurros@gmail.com>
2022-06-29 20:22:09 -0400

    testing ticketmaster api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
-var getUserRepos = function(user) {
+
+
+
+var getUserRepos = function() {
     // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
+   var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=PEXCtwTSHSAIjA1qUOIJkDGGqhUR7GPo"
+    // var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=PEXCtwTSHSAIjA1qUOIJkDGGqhUR7GPo&stateCode=VA"
+  console.log(apiUrl)
     // make a get request to url
-    fetch(apiUrl).then(function(response) {
-      console.log(response);
-      response.json().then(function(data) {
+    fetch(apiUrl)
+    .then(function(response) {
+      
+      return response.json()
+      .then(function(data) {
         console.log(data);
       });
     });
   };
   
-  getUserRepos("lernantino");
+  getUserRepos(22202);
   
