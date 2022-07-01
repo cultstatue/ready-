98ffba259bced4d313ae8ab2434dfff2f69d926b
kea6t <keasay26@gmail.com>
2022-07-01 13:31:48 -0400

    working version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
 var apiKey = "PEXCtwTSHSAIjA1qUOIJkDGGqhUR7GPo";
 
-var getUserRepos = function(city) {
+var getEventsApi = function(city) {
     // format the github api url
-    var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?"  + city + "&appid=" + apiKey;
-  
+   var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + "&city=" + city;
+    // var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=PEXCtwTSHSAIjA1qUOIJkDGGqhUR7GPo&stateCode=VA"
+  console.log(apiUrl)
     // make a get request to url
-    fetch(apiUrl).then(function(response) {
+    fetch(apiUrl)
+    .then(function(response) {
       
-      return response.json().then(function(data) {
+      return response.json()
+      .then(function(data) {
         console.log(data);
       });
     });
   };
   
-  getUserRepos("Boston");
+  getEventsApi("Boston");
   
