f1c0e481e73bd741f35fa931782765cbd706e384
kea6t <keasay26@gmail.com>
2022-07-02 15:27:23 -0400

    we added a display events for current events happening now and future events. -KA

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var apiKey = "PEXCtwTSHSAIjA1qUOIJkDGGqhUR7GPo";
 var searchBox = document.querySelector('#user-form');
 var cityInputEl = document.querySelector('#city-input');
 var cityContainerEl = document.querySelector('#city-container');
+var mainContainerEl = document.querySelector('#main-container');
+
 
 searchBox.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -15,7 +17,7 @@ searchBox.addEventListener("submit", function (event) {
 
 var getEventsApi = function (city) {
     // format the github api url
-     var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + "&city=" + city;
+    var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + "&city=" + city;
     // var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=PEXCtwTSHSAIjA1qUOIJkDGGqhUR7GPo&postalCode="
     console.log(apiUrl)
     // make a get request to url
@@ -24,7 +26,7 @@ var getEventsApi = function (city) {
 
             return response.json()
                 .then(function (data) {
-                    console.log(data);
+                    displayEvents(data);
                 });
         });
 };
@@ -81,4 +83,15 @@ var getWeatherdata = function (lat, lon, cityName) {
             alert("Unable to connect to OpenWeather");
 
         });
+}
+
+var displayEvents = function (eventData) {
+    console.log(eventData);
+    for (var i = 0; i < eventData._embedded.events.length; i++) {
+        var eventCard = document.createElement("div");
+        eventCard.className = "card";
+        eventCard.innerHTML = "<div class='card-image'><figure class='image is-4by3'><img src=" + eventData._embedded.events[i].images[0].url + " alt='Placeholder image'></figure></div><div class='content'>Name of event: " + eventData._embedded.events[i].name + " <br> Date of event: " + eventData._embedded.events[i].dates.start.localDate +" <br> Event Type: " + eventData._embedded.events[i]._embedded.venues[0].address.line1 + " <br> Event Type: " + eventData._embedded.events[i].priceRanges[0].min + "-" + eventData._embedded.events[i].priceRanges[0].max +" <br> buy your tickets here: <a href=" + eventData._embedded.events[i].url + ">Click here </a> </div></div>";
+        
+        mainContainerEl.appendChild(eventCard); 
+    }
 }
